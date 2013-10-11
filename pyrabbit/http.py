eba75cc39d4b0ff3cfd2fd10966b11f354dda4ea
Thomas French <thomas@sandtable.com>
2013-10-11 15:20:22 +0100

    use HTTPS

--- pyrabbit/http.py
+++ pyrabbit/http.py
@@ -62,7 +62,7 @@ class HTTPClient(object):
 
         self.client = httplib2.Http(timeout=timeout)
         self.client.add_credentials(uname, passwd)
-        self.base_url = 'http://%s/api' % server
+        self.base_url = 'https://%s/api' % server
 
     def decode_json_content(self, content):
         """
